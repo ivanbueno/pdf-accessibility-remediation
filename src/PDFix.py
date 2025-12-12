ad2164a656b88f9d68ce49d4e9e960a924628299
Ivan Bueno <ivanbueno@gmail.com>
2025-12-12 00:06:12 -0800

    update actions

--- src/PDFix.py
+++ src/PDFix.py
@@ -1,14 +1,10 @@
-from Utils import inputPath, outputPath
 from pdfixsdk import *
-from Validation import validatePdf
-from pathlib import Path
-import multiprocessing
-import time
-
-inputFolder = inputPath + "/judicial-council-pdfs"
-outputFolder = outputPath + "/judicial-council-pdfs-accessible"
+from Utils import inputPath, outputPath
 
 def FixPDF(inputPdfPath: str, outputPdfPath: str):
+    # print(f"Remediating: {inputPdfPath}")
+    commandPath = inputPath + "/make-accessible.json"
+
     # Open the PDF document
     pdfix  = GetPdfix()
     if pdfix is None:
@@ -20,7 +16,8 @@ def FixPDF(inputPdfPath: str, outputPdfPath: str):
 
     command = doc.GetCommand()
 
-    cmdStm = pdfix.CreateMemStream()
+    cmdStm = pdfix.CreateFileStream(commandPath, kPsReadOnly)
+    # cmdStm = pdfix.CreateMemStream()
     if not command.SaveCommandsToStream(kActionMakeAccessible, cmdStm, kDataFormatJson, kSaveFull):
         print(pdfix.GetError())
 
@@ -37,47 +34,4 @@ def FixPDF(inputPdfPath: str, outputPdfPath: str):
         print(pdfix.GetError())
 
     doc.Close()
-
-if __name__ == '__main__':
-
-    input_files = []
-    input_outputs = []
-    for pdfFile in Path(inputFolder).glob("*.pdf"):
-        input_files.append(str(pdfFile))
-        input_outputs.append((str(pdfFile), str(Path(outputFolder) / pdfFile.name)))
-
-    print("Starting validation before remediation...")
-    start_wall = time.perf_counter()
-    start_cpu = time.process_time()
-    with multiprocessing.Pool(processes=4) as pool:
-        results = pool.map(validatePdf, input_files)
-    print(f"VeraPDF Validation - Passed: {results.count(True)}, Failed: {results.count(False)}")
-    end_wall = time.perf_counter()
-    end_cpu = time.process_time()
-    print(f"{end_wall - start_wall:.2f} seconds (wall time)")
-    print(f"{end_cpu - start_cpu:.2f} seconds (CPU time)")
-
-    print("PDFix remediation...")
-    start_wall = time.perf_counter()
-    start_cpu = time.process_time()
-    with multiprocessing.Pool(processes=4) as pool:
-        results = pool.starmap(FixPDF, input_outputs)
-    end_wall = time.perf_counter()
-    end_cpu = time.process_time()
-    print(f"{end_wall - start_wall:.2f} seconds (wall time)")
-    print(f"{end_cpu - start_cpu:.2f} seconds (CPU time)")
-    print("Remediation completed.")
-
-    print("Starting validation after remediation...")
-    output_files = []
-    start_wall = time.perf_counter()
-    start_cpu = time.process_time()
-    for pdfFile in Path(outputFolder).glob("*.pdf"):
-        output_files.append(str(pdfFile))
-    with multiprocessing.Pool(processes=4) as pool:
-        results = pool.map(validatePdf, output_files)
-    print(f"VeraPDF Validation - Passed: {results.count(True)}, Failed: {results.count(False)}")
-    end_wall = time.perf_counter()
-    end_cpu = time.process_time()
-    print(f"{end_wall - start_wall:.2f} seconds (wall time)")
-    print(f"{end_cpu - start_cpu:.2f} seconds (CPU time)")
+    # print(f"Remediation completed: {outputPdfPath}")
